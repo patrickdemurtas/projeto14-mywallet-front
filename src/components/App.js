963afacd0a20de8a0bb02012c2e7642217c2b241
patrickdemurtas <patrickmurtass@gmail.com>
2023-01-21 17:59:09 -0300

    inicio

---
+++ src/components/App.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState } from "react";
+
+
+function App() {
+    return(
+          <Container>
+
+          </Container>
+    )
+}
+
+export default App
+
+const Container = styled.div`
+width: 375px;
+height: 667px;
+background-color: #9567BE;
+margin: 0 auto;
+display: flex;
+flex-direction: column;
+`
